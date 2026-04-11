9437b492df7787e840cf522518b54a100f3fd102
Ujjwal Kaushik <ujjwalkaus21@gmail.com>
2026-04-11 19:51:15 +0530

    Queue

---
+++ src/Tree/findMax.java
@@ -0,0 +1,34 @@
+package Tree;
+
+import org.w3c.dom.Node;
+
+public class findMax {
+    public static void main(String[] args) {
+        
+
+    }
+    public int maxTree(){
+        return maxTree(root);
+    }
+
+    private static int maxTree(Node root) {
+        if(root == null) return Integer.MIN_VALUE;
+
+        int lmax = maxTree(root.left);
+        int rmax = maxTree(root.right);
+
+        return Math.max(lmax,Math.max(rmax,root.data));
+    }
+
+    public boolean find(int val){
+        return find(root,val);
+    }
+    private boolean find(Node root, int val) {
+        if (root == null) return false;
+
+        
+        if (root.data == val) return true;
+        
+        return find(root.left, val) || find(root.right, val);
+    }
+}
