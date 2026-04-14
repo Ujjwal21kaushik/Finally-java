890e4c0102867256f391cb93483909c82c51720e
Ujjwal Kaushik <ujjwalkaus21@gmail.com>
2026-04-14 16:51:42 +0530

    Implement MyQueue using two stacks with push, pop, peek, and empty methods

---
+++ src/queuessss/ImplementationUsingTwoStack.java
@@ -0,0 +1,59 @@
+package queuessss;
+
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> st;
+    Stack<Integer> helper;
+
+    public MyQueue() {
+        st = new Stack<>();
+        helper = new Stack<>();
+    }
+    
+    public void push(int x) {
+        st.push(x);
+    }
+    
+    public int pop() {
+       while(st.size()>1){
+        helper.push(st.pop());
+       }
+       int front = st.pop();
+       while(helper.size()>0){
+        st.push(helper.pop());
+       }
+       return front;
+    }
+    
+    public int peek() {
+        while(st.size()>1){
+        helper.push(st.pop());
+       }
+       int front = st.peek();
+       while(helper.size()>0){
+        st.push(helper.pop());
+       }
+       return front;
+    }
+    
+    public boolean empty() {
+        return (st.size()==0 && helper.size()==0);
+    }
+}
+public class ImplementationUsingTwoStack {
+    
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.push(10);
+        q.push(20);
+        q.push(30);
+        q.push(40);
+
+        System.out.println(q.peek());
+        System.out.println(q.pop());
+        System.out.println(q.peek());
+    }
+
+}
