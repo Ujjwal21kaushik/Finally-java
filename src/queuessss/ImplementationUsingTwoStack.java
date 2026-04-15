490e347d0913725b6fe6ce547b42cd2e3b79ad7c
Ujjwal Kaushik <ujjwalkaus21@gmail.com>
2026-04-15 17:46:25 +0530

    Refactor MyQueue to MyStackQueue and update push, pop, and peek methods for improved functionality

--- src/queuessss/ImplementationUsingTwoStack.java
+++ src/queuessss/ImplementationUsingTwoStack.java
@@ -2,42 +2,64 @@ package queuessss;
 
 import java.util.Stack;
 
-class MyQueue {
+class MyStackQueue {
 
     Stack<Integer> st;
     Stack<Integer> helper;
 
-    public MyQueue() {
+    public MyStackQueue() {
         st = new Stack<>();
         helper = new Stack<>();
     }
     
-    public void push(int x) {
-        st.push(x);
-    }
+    // public void push(int x) {
+    //     st.push(x);
+    // }
     
+    // public int pop() {
+    //    while(st.size()>1){
+    //     helper.push(st.pop());
+    //    }
+    //    int front = st.pop();
+    //    while(helper.size()>0){
+    //     st.push(helper.pop());
+    //    }
+    //    return front;
+    // }
+    
+    // public int peek() {
+    //     while(st.size()>1){
+    //     helper.push(st.pop());
+    //    }
+    //    int front = st.peek();
+    //    while(helper.size()>0){
+    //     st.push(helper.pop());
+    //    }
+    //    return front;
+    // }
+        // public void push(int x) {
+    //     st.push(x);
+    // }
+    // *-*-*-**********-*--*-** Approch 2 ****-*-**-*-*-*-*-*-***********
+    public void push(int x) {
+        while(!st.empty()){
+            helper.push(st.pop());
+        }
+        st.push(x);
+        while(!helper.empty()){
+            st.push(helper.pop());
+        }
+
+    }
     public int pop() {
-       while(st.size()>1){
-        helper.push(st.pop());
-       }
-       int front = st.pop();
-       while(helper.size()>0){
-        st.push(helper.pop());
-       }
-       return front;
+       
+       return st.pop();
     }
     
     public int peek() {
-        while(st.size()>1){
-        helper.push(st.pop());
-       }
-       int front = st.peek();
-       while(helper.size()>0){
-        st.push(helper.pop());
-       }
-       return front;
+        
+       return st.peek();
     }
-    
     public boolean empty() {
         return (st.size()==0 && helper.size()==0);
     }
@@ -45,7 +67,7 @@ class MyQueue {
 public class ImplementationUsingTwoStack {
     
     public static void main(String[] args) {
-        MyQueue q = new MyQueue();
+        MyStackQueue q = new MyStackQueue();
         q.push(10);
         q.push(20);
         q.push(30);
